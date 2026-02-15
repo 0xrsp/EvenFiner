3f74c5c48026a8cb985add3e9848ac41270fcde3
mmmm <95966883+0xrsp@users.noreply.github.com>
2026-02-15 13:33:41 +1030

    clean build script

--- clean.bat
+++ clean.bat
@@ -1,14 +1,7 @@
 @echo off
 echo Cleaning...
 
-for /r %%f in (*.pch *.ilk *.pdb *.obj *.asm) do (
-  if exist "%%f" (
-    del /Q "%%f"
-    echo Deleting %%f
-  )
-)
-
-for /r %%f in (*.exe) do (
+for /r %%f in (*.pch *.ilk *.pdb *.obj *.exe) do (
   del /Q "%%f"
   echo Deleting %%f
 )
